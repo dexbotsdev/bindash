e1a0eff869b0056aa99fb7075b233a9c7d853555
DefiCody <ambergridpro@gmail.com>
2023-06-11 00:03:02 +0530

    w

---
+++ src/config/api-keys.js
@@ -0,0 +1,10 @@
+export const CONFIGURATION = {
+  binance: {
+    key: 'JwenEFN2jDoADT2j4de7vfbjouxsBWjwIFTYihaVMQpmyoVYJbDP4dipMCKygFgt',
+    secret: '0HxPpM4Co7Q7yEqhfPBZHpTJ6jhQiUKZr4283o65EFVsTbx5mFDMh2bhql3UnPcQ',
+    exchangeType: 'futures' // spot is not supported yet
+  },
+  profile: {
+    name: 'Jerry Frye'
+  }
+};
